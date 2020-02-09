a2e0ba933bb6722db095617316b91918cb9e5005
Jim Clark <slimslenderslacks@gmail.com>
2020-02-09 15:13:05 -0800

    cleanup

--- dev/lumo-compile.cljs
+++
@@ -1,5 +0,0 @@
-(require '[lumo.build.api])
-(lumo.build.api/build "src" {:main 'editors.core
-                             :output-to "index.js"
-                             :optimizations :none
-                             :target :nodejs})
